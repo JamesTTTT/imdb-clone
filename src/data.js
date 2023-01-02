058c544b4e439a0e9eb61d45d08bfa44fbd48670
James Taylor <jata20@student.bth.se>
2023-01-02 21:02:42 +0000

    Added file for storing data arrays

---
+++ src/data.js
@@ -0,0 +1,13 @@
+import { AiOutlineHome, AiOutlineCompass } from "react-icons/ai";
+export const sidebarData = [
+  {
+    title: "Home",
+    directory: "/",
+    icon: <AiOutlineHome />,
+  },
+  {
+    title: "Discover",
+    directory: "/discover",
+    icon: <AiOutlineCompass />,
+  },
+];
