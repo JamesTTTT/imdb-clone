e65ecf9f176377191f7c0d39332224bf9ef4d223
James Taylor <jata20@student.bth.se>
2023-01-03 23:59:00 +0000

    Added Trending and Movielist components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { MovieList, Trending } from "../components";
+import tmdb from "../models/tmdb";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [trending, setTrending] = useState();
+  const [nowPlaying, setNowPlaying] = useState();
+  const [topRated, setTopRated] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const trendingRes = await tmdb.getTrending();
+      const nowPlayingRes = await tmdb.getNowPlaying();
+      const topRated = await tmdb.getTopRated();
+      setTrending(trendingRes.results);
+      setNowPlaying(nowPlayingRes.results);
+      setTopRated(topRated.results);
+    }
 
-export default Home
+    fetchData();
+  }, []);
+
+  return (
+    <div className="home">
+      <Trending data={trending} />
+      <MovieList data={nowPlaying} name={"Now Playing"} />
+      <MovieList data={topRated} name={"Top Rated"} />
+    </div>
+  );
+};
+
+export default Home;
