0a5999dc1b5253d8baf27d3df09993a1711cd69a
James Taylor <jata20@student.bth.se>
2023-01-03 23:57:32 +0000

    Added Footer and Searchbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Sidebar } from "./components";
+import { Sidebar, Footer, Searchbar } from "./components";
 import { Home, Discover } from "./pages";
 import "./style/App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="page">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/discover" element={<Discover />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="page">
+          <Sidebar />
+          <div className="content">
+            <Searchbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/discover" element={<Discover />} />
+            </Routes>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 };
 
