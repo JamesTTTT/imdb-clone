8991502a413c5504b598cc340cb6e7ab60d715ea
James Taylor <jata20@student.bth.se>
2023-01-03 23:58:07 +0000

    Export new components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,5 @@
 export { default as Sidebar } from "./Sidebar";
+export { default as Footer } from "./Footer";
+export { default as Searchbar } from "./Searchbar";
+export { default as Trending } from "./Home/Trending";
+export { default as MovieList } from "./Home/MovieList";
