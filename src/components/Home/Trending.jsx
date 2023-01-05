9610f314c952fabf19fa8c1b4726d9d4123b229f
James Taylor <jata20@student.bth.se>
2023-01-05 12:56:54 +0000

    Updated css for trending tabs

--- src/components/Home/Trending.jsx
+++ src/components/Home/Trending.jsx
@@ -1,23 +1,67 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 const Trending = ({ data }) => {
+  const navigate = useNavigate();
+  const [boxIndex, setBoxIndex] = useState(1);
+
+  const handleDirection = (direction) => {
+    if (direction === "Right") {
+      if (boxIndex < data.length - 1) {
+        setBoxIndex(boxIndex + 1);
+      } else {
+        setBoxIndex(1);
+      }
+    }
+
+    if (direction === "Left") {
+      if (boxIndex > 1) {
+        setBoxIndex(boxIndex - 1);
+      } else {
+        setBoxIndex(data.length - 1);
+      }
+    }
+  };
+
   const trendingBoxes = () => {
-    return data.map((item, index) => {
-      return (
+    return (
+      <div className="trendingContainer">
         <div
-          key={index}
           className="trendingBox"
+          onClick={() => {
+            navigate("/inspect", {
+              state: { id: data[boxIndex - 1].id },
+            });
+          }}
           style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w780${item.backdrop_path})`,
+            marginRight: 5,
+            backgroundImage: `url(https://image.tmdb.org/t/p/w780${
+              data[boxIndex - 1].backdrop_path
+            })`,
           }}
         >
           <div className="titleBox">
-            <p>{item.title}</p>
-            <p>{item.release_date}.</p>
+            <p>{data[boxIndex - 1].title}</p>
+            <p>{data[boxIndex - 1].release_date}.</p>
           </div>
         </div>
-      );
-    });
+        <div
+          className="trendingBox"
+          onClick={() => {
+            navigate("/inspect", { state: { id: data[boxIndex].id } });
+          }}
+          style={{
+            marginLeft: 5,
+            backgroundImage: `url(https://image.tmdb.org/t/p/w780${data[boxIndex].backdrop_path})`,
+          }}
+        >
+          <div className="titleBox">
+            <p>{data[boxIndex].title}</p>
+            <p>{data[boxIndex].release_date}.</p>
+          </div>
+        </div>
+      </div>
+    );
   };
   //If no data then Loading
   if (!data) {
@@ -26,7 +70,25 @@ const Trending = ({ data }) => {
   return (
     <div className="component">
       <h3>Trending</h3>
-      <div className="trendingContainer">{trendingBoxes()}</div>
+      <div className="flex-row">
+        <button
+          className="arrow arrow-left"
+          onClick={() => {
+            handleDirection("Left");
+          }}
+        >
+          <AiOutlineLeft />
+        </button>
+        <div>{trendingBoxes()}</div>
+        <button
+          className="arrow arrow-right"
+          onClick={() => {
+            handleDirection("Right");
+          }}
+        >
+          <AiOutlineRight />
+        </button>
+      </div>
     </div>
   );
 };
