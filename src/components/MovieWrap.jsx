0f979ddb168446a081b0738c107755e1789bc96b
James Taylor <jata20@student.bth.se>
2023-01-05 12:47:54 +0000

    Added component to replace MovieList

---
+++ src/components/MovieWrap.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const MovieWrap = ({ data, name, max }) => {
+  const navigate = useNavigate();
+  const movieBoxes = () => {
+    data = data.slice(0, max);
+    return data.map((item, index) => {
+      return (
+        <div
+          key={index}
+          className="posterBox"
+          onClick={() => {
+            navigate("/inspect", { state: { id: item.id } });
+          }}
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w185${item.poster_path})`,
+            cursor: "pointer ",
+          }}
+        >
+          <div className="movie-details">
+            <p>{item.title}</p>
+            <p>{item.release_date}</p>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  //If no data then Loading
+  if (!data) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div className="component">
+      <h3>{name}</h3>
+      <div className="movieContainer">{movieBoxes()}</div>
+    </div>
+  );
+};
+
+export default MovieWrap;
