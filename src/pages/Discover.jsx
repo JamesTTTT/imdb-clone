0e8fc84125dd1f0331a94a99a469e50552d063e6
James Taylor <jata20@student.bth.se>
2023-01-05 12:52:52 +0000

    Added discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import tmdb from "../models/tmdb";
+import { Genres, MovieWrap } from "../components";
 const Discover = () => {
-  return (
-    <div>Discover</div>
-  )
-}
+  const [genres, setGenres] = useState();
+  const [selected, setSelected] = useState();
+  const [selectedGenreData, setSelectedGenreData] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const genres = await tmdb.getGenres(selected);
+      console.log(genres);
+      setGenres(genres.genres);
+    }
 
-export default Discover
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const genreData = await tmdb.getMoviesByGenres(selected);
+      setSelectedGenreData(genreData.results);
+    }
+
+    fetchData();
+  }, [selected]);
+
+  return (
+    <div>
+      <Genres data={genres} setSelected={setSelected} />
+      <MovieWrap data={selectedGenreData} />
+    </div>
+  );
+};
+
+export default Discover;
