96e4b5974f766c399814d708024f1f05ef3a3b1d
James Taylor <jata20@student.bth.se>
2023-01-12 01:02:05 +0000

    Added exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,9 @@
 export { default as Sidebar } from "./Sidebar";
 export { default as Footer } from "./Footer";
-export { default as Searchbar } from "./Searchbar";
+export { default as Header } from "./Header";
+export { default as Searchbar } from "./Search/Searchbar";
+
 export { default as Trending } from "./Home/Trending";
-//export { default as MovieList } from "./Home/MovieList";
 export { default as MovieWrap } from "./MovieWrap";
 export { default as Genres } from "./Discover/Genres";
+export { default as FilterDiscover } from "./Discover/FilterDiscover";
