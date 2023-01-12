1799e4e1a3b217c3b8ec184ff9537b3ed8ef4e48
James Taylor <jata20@student.bth.se>
2023-01-12 01:02:32 +0000

    Edited inspect page

--- src/pages/Inspect.jsx
+++ src/pages/Inspect.jsx
@@ -7,12 +7,16 @@ const Inspect = () => {
   const [movieDetails, setMovieDetails] = useState();
   const [movieVideo, setMovieVideo] = useState();
   const [isWatchTrailer, setIsWatchTrailer] = useState(false);
+  const [cast, setCast] = useState();
   useEffect(() => {
     async function fetchData() {
       const movieData = await tmdb.getMovieDetails(id);
       const videoData = await tmdb.getMovieVideo(id);
+      const castData = await tmdb.getCredits(id);
+      setCast(castData.cast);
       setMovieDetails(movieData);
       setMovieVideo(videoData.results[0]);
+      console.log(movieData);
     }
 
     fetchData();
@@ -28,7 +32,22 @@ const Inspect = () => {
     });
   };
 
-  if (!movieDetails || !movieVideo) return <div>No Data</div>;
+  const castList = () => {
+    return cast.map((item, index) => {
+      return (
+        <div key={index} className="cast-box">
+          <img
+            width="98"
+            src={`https://image.tmdb.org/t/p/w185${item.profile_path}`}
+          />
+          <p>{item.original_name}</p>
+          <p className="character">{item.character}</p>
+        </div>
+      );
+    });
+  };
+
+  if (!movieDetails || !cast) return <div>Loading</div>;
 
   return (
     <div className="inspect">
@@ -37,25 +56,32 @@ const Inspect = () => {
         <p>{movieDetails.release_date}</p>
         <div className="genre-felx">{genres()}</div>
       </div>
-      {isWatchTrailer ? (
+      {isWatchTrailer && movieVideo ? (
         <iframe
-          width="780"
-          height="440"
-          src={`https://www.youtube.com/watch?v=${movieVideo.key}`}
+          height="400"
+          className="frame"
+          src={`https://www.youtube.com/embed/${movieVideo.key}`}
         ></iframe>
       ) : (
         <img
-          width="780"
-          height="440"
+          className="frame"
           src={`https://image.tmdb.org/t/p/w780${movieDetails.backdrop_path}`}
           alt={movieDetails.title}
         />
       )}
 
-      <div className="description-box">
-        <h4>Description</h4>
+      <div className="information-box">
+        <div className="description-box">
+          <div>
+            <div>
+              <p>Score: {movieDetails.vote_average.toFixed(2)}</p>
+            </div>
 
-        <p>{movieDetails.overview}</p>
+            <h4>Description</h4>
+
+            <p>{movieDetails.overview}</p>
+          </div>
+        </div>
         <button
           onClick={() => {
             setIsWatchTrailer(true);
@@ -63,6 +89,8 @@ const Inspect = () => {
         >
           Watch Trailer
         </button>
+        <h4>Cast</h4>
+        <div className="cast-list">{castList()}</div>
       </div>
     </div>
   );
