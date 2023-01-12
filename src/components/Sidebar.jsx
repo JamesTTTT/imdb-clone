a5a98da0ea1110503a606dc986575c3f1825ae99
James Taylor <jata20@student.bth.se>
2023-01-12 01:08:53 +0000

    Made sclae changes to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../style/App.css";
 import { NavLink } from "react-router-dom";
 import { sidebarData } from "../data";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
+  const { activeMenu, setActiveMenu, screenSize, setScreenSize } =
+    useStateContext();
+
   const activeLink = "activeLink";
   const normalLink = "normalLink";
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
   const SidebarItems = () => {
     return sidebarData.map((item) => {
@@ -13,6 +40,7 @@ const Sidebar = () => {
         <NavLink
           key={item.title}
           to={item.directory}
+          onClick={handleCloseSideBar}
           className={({ isActive }) => (isActive ? activeLink : normalLink)}
         >
           <div className="navbox">
@@ -24,9 +52,13 @@ const Sidebar = () => {
     });
   };
 
+  if (!activeMenu) {
+    return null;
+  }
+
   return (
     <nav className="sidebar">
-      <h3>Movie</h3>
+      {screenSize > 900 ? <h3>Movie</h3> : null}
       {SidebarItems()}
     </nav>
   );
