509c64440127389dfdf90d197a802c8bf4a891a6
James Taylor <jata20@student.bth.se>
2023-01-12 01:02:57 +0000

    Added filters to discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import tmdb from "../models/tmdb";
-import { Genres, MovieWrap } from "../components";
+import { Genres, MovieWrap, FilterDiscover } from "../components";
 const Discover = () => {
   const [genres, setGenres] = useState();
   const [selected, setSelected] = useState();
   const [selectedGenreData, setSelectedGenreData] = useState();
+  const [score, setScore] = useState([2.0, 10.0]);
+  const [sort, setSort] = useState("popularity");
   useEffect(() => {
     async function fetchData() {
       const genres = await tmdb.getGenres(selected);
-      console.log(genres);
       setGenres(genres.genres);
     }
 
@@ -17,15 +18,21 @@ const Discover = () => {
 
   useEffect(() => {
     async function fetchData() {
-      const genreData = await tmdb.getMoviesByGenres(selected);
+      const genreData = await tmdb.getMoviesByGenres(selected, score, sort);
       setSelectedGenreData(genreData.results);
     }
 
     fetchData();
-  }, [selected]);
+  }, [selected, score, sort]);
 
   return (
     <div>
+      <FilterDiscover
+        setScore={setScore}
+        setSort={setSort}
+        score={score}
+        sort={sort}
+      />
       <Genres data={genres} setSelected={setSelected} />
       <MovieWrap data={selectedGenreData} />
     </div>
